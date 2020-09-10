78920dfa292ad7f5085f2808f74985bb22ba86eb
yiweizhang <yiweizhang@ruc.edu.cn>
2020-09-10 17:11:34 +0800

    add main function

--- src/com/company/test.java
+++ src/com/company/test.java
@@ -1,4 +1,10 @@
 package com.company;
 
 public class test {
+    public static void main(String[] args){
+        System.out.println("this is main function");
+    }
+    public void filetest(){
+        System.out.println("hello");
+    }
 }
