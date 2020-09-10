0d4f1bcddae764c8bea031d7673d87e843627e5e
yiweizhang <yiweizhang@ruc.edu.cn>
2020-09-10 16:48:08 +0800

    update test file

--- src/com/company/test.java
+++ src/com/company/test.java
@@ -4,4 +4,8 @@ public class test {
     public void fileTest(int a){
         System.out.println(a > 2 ? "hello" : "world");
     }
+
+    public int calc(int a, int b){
+        return a > b ? 2 : 3;
+    }
 }
