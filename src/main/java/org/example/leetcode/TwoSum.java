3f6c690123e146ee0044e90f18da2f350be880cf
时新 <xiangyuxi.xyx@alibaba-inc.com>
2023-12-28 22:56:36 +0800

    update java8

--- src/main/java/org/example/leetcode/TwoSum.java
+++ src/main/java/org/example/leetcode/TwoSum.java
@@ -65,6 +65,31 @@ public class TwoSum {
         nums[end] = temp;
     }
 
+
+    //【搜索插入的位置】给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+    public static int searchInsert(int[] nums, int target) {
+        int p = 0, q = nums.length-1;
+        int mid = -1;
+        if (nums.length == 1){
+            if (nums[0] == target){
+                return 0;
+            }
+            return nums[0] > target ? 0 : 1;
+        }
+        while (p < q) {
+            mid = (p+q)/2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            if (nums[mid] > target){
+                q = mid-1;
+            }else {
+                p = mid+1;
+            }
+        }
+        return mid;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{1,2,5,7,7,7};
         int[] ans = searchRange(nums, 8);
